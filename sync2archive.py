2b679b73c4eb81da61fdee7387f583341e61d1c2
Romain <romain.bertolini@gmail.com>
2023-11-29 12:26:37 -0800

    Added limit, move of file and other

--- sync2archive.py
+++ sync2archive.py
@@ -1,20 +1,19 @@
 from pathlib import Path
 import argparse
 from PIL import Image, ExifTags, UnidentifiedImageError
-from datetime import datetime, timezone
-from dateutil.parser import parse
-from datetime import timedelta
 import pendulum
 
-d = timedelta(weeks=4)
 
-archive_limit = pendulum.instance(datetime.now() - d)
-print(archive_limit)
+nb_moved_files = {}
 
 def is_file_archivable(file, date):
-    print(f"{date} < {archive_limit}")
     return date < archive_limit
-        
+
+def count_moved_files(folder_name):
+    if folder_name in nb_moved_files.keys():
+        nb_moved_files[folder_name] += 1
+    else:
+        nb_moved_files[folder_name] = 1
 
 parser = argparse.ArgumentParser(
                     prog='Sync2Archive',
@@ -22,9 +21,13 @@ parser = argparse.ArgumentParser(
                     epilog='')
 
 parser.add_argument('syncfolder')           # positional argument
-#parser.add_argument('destfolder')           # positional argument
+parser.add_argument('destfolder')           # positional argument
+parser.add_argument('-f', '--force', action='store_false')
+parser.add_argument('-l', '--limit', type = int, default = 4, help="limit in month before now for the files to be archived.")
 
 args = parser.parse_args()
+
+archive_limit = pendulum.now().subtract(months = args.limit)
 
 p = Path(args.syncfolder)
 files = [x for x in p.iterdir() if x.is_file()]
@@ -41,8 +44,18 @@ for f in files:
         continue
 
     date = pendulum.parse(datestr)
-    #print(f"{date}    {datestr}")
-    
-    print(f"{f} is archivable : {is_file_archivable(f, date)}")
 
+    if is_file_archivable(f, date):
+        folder_name = f"{date.year}-{date.month:02d}"
+        archive_folder = Path(args.destfolder, folder_name)
+        archive_path = Path(archive_folder, f.name)
 
+        count_moved_files(folder_name)
+        print(f"Move {f} to {archive_path}")
+
+        if args.force:
+            archive_folder.mkdir(parents = True, exist_ok=True)
+            f.rename(archive_path)   
+
+for folder, nb in nb_moved_files.items():
+    print(f"Moved {nb} files to {folder}")
